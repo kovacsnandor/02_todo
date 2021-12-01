44f85dcf5a5e59cc159b1b212cbe299f08470d6d
kovacsnandor <kovacsnandor@gmail.com>
2021-12-01 18:34:57 +0100

    Todo soztállyal

--- my.js
+++ my.js
@@ -1,12 +1,20 @@
+class Todo {
+    constructor(name) {
+        this.name = name;
+        this.completed = false;
+        this.editing = false;
+    }
+}
+
 var app = new Vue({
     el: '#app',
     data: {
       message: 'Hello Vue!',
       todoCollection: [
-          "todo 1",
-          "todo 2",
-          "todo 3",
-          "todo 4"
+          new Todo("todo 1"),
+          new Todo("todo 2"),
+          new Todo("todo 3"),
+          new Todo("todo 4")
       ]
     }
   })
