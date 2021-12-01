6ffaa152564f7f2773010ba3a6d9fb8167091b80
kovacsnandor <kovacsnandor@gmail.com>
2021-12-01 18:26:50 +0100

    for szemléltetés

--- my.js
+++ my.js
@@ -1,6 +1,12 @@
 var app = new Vue({
     el: '#app',
     data: {
-      message: 'Hello Vue!'
+      message: 'Hello Vue!',
+      todoCollection: [
+          "todo 1",
+          "todo 2",
+          "todo 3",
+          "todo 4"
+      ]
     }
   })
