5c5452b860611aa6d9d6164a9b6552aaf9b9c8c4
kovacsnandor <kovacsnandor@gmail.com>
2021-12-01 19:38:12 +0100

    New todo, Enter todo

--- my.js
+++ my.js
@@ -10,6 +10,7 @@ var app = new Vue({
     el: '#app',
     data: {
       message: 'Hello Vue!',
+      newTodoName: null,
       todoCollection: [
           new Todo("todo 1"),
           new Todo("todo 2"),
@@ -20,6 +21,15 @@ var app = new Vue({
     methods: {
         onDblClickTodoName(todo){
             todo.editing = true;
-        }
+        },
+        OnEnterTodoName(todo){
+            todo.editing = false;
+        },
+        OnEnterAddTodo(){
+            if (this.newTodoName) {
+                this.todoCollection.push(new Todo(this.newTodoName));
+                this.newTodoName = "";
+            }
+        },
     }
   })
