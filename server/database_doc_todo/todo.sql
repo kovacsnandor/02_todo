99cc618b863ddf5e7887d7a83cb5d0779da99df8
kovacsnandor <kovacsnandor@gmail.com>
2023-03-10 08:14:51 +0100

    javítás

--- server/database_doc_todo/todo.sql
+++ server/database_doc_todo/todo.sql
@@ -1,4 +1,26 @@
+DELETE FROM todos;
+
 INSERT INTO todos
-  ()
+  (id, name, completed, editing)
   VALUES
-  ()
+  (1, 'mozi', 0,0), (2, 'sörözõ', 0,0), (3, 'borozó', 0,0);
+
+
+#lekérdezések
+select * from todos;
+
+# post
+INSERT INTO todos
+  (name, completed)
+  VALUES
+  ('mozi', 0);
+
+# delete completed
+DELETE FROM todos
+  WHERE completed = 1;
+
+#put
+UPDATE todos SET
+  name = 'mozi', completed = 1
+  WHERE id = 1;
+
