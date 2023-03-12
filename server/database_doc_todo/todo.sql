4edccd3c69fb1c5ac804c52985c5a8fb221b9430
kovacsnandor <kovacsnandor@gmail.com>
2023-03-12 20:54:37 +0100

    a user tábla beillesztése

--- server/database_doc_todo/todo.sql
+++ server/database_doc_todo/todo.sql
@@ -1,3 +1,28 @@
+# users tábla
+# users tábla létrehozása
+CREATE TABLE todo.users (
+  id INT(11) NOT NULL AUTO_INCREMENT,
+  firstName VARCHAR(50) DEFAULT NULL,
+  lastName VARCHAR(50) DEFAULT NULL,
+  gender VARCHAR(10) DEFAULT NULL,
+  userName VARCHAR(50) DEFAULT NULL,
+  email VARCHAR(255) DEFAULT NULL,
+  password VARCHAR(100) DEFAULT NULL,
+  number INT(11) DEFAULT NULL,
+  PRIMARY KEY (id)
+)
+ENGINE = INNODB,
+AUTO_INCREMENT = 23,
+AVG_ROW_LENGTH = 5461,
+CHARACTER SET utf8,
+COLLATE utf8_hungarian_ci;
+
+ALTER TABLE todo.users 
+  ADD UNIQUE INDEX UK_users_email(email);
+
+
+
+
 DELETE FROM todos;
 
 INSERT INTO todos
